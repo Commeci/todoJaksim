c6fca1bab7f4cd6f3dc1d6ea7db33b6fd8f2424f
Commeci <cjtkemfldj@naver.com>
2024-08-23 11:36:16 +0900

    index.js 수정

--- index.js
+++ index.js
@@ -25,9 +25,13 @@ async function start() {
     const client = await connectDB();
     await Post.injectDB(client);
 
-    app.listen(3000, () => {
-        console.log("서버 실행중...");
-    });
+    if (process.env.VERCEL) {
+        console.log("Vercel 환경에서는 서버를 시작하지 않습니다.");
+    } else {
+        app.listen(port, () => {
+            console.log("서버 실행중...");
+        });
+    }
 
     process.on("SIGINT", async () => {
         try {
